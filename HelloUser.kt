591b14b8073e984aef9fb5840a94009402e65093
PKrystian <krystian.pinczak2001@gmail.com>
2023-10-11 13:15:07 +0200

    dev, helloUser program improvements

--- HelloUser.kt
+++ HelloUser.kt
@@ -1,6 +1,10 @@
+/*
+    Write a program that displays the text "Hello <name>!" in the console,
+    where <name> is the user's name (ask for it in the console).
+ */
 fun main() {
-    print("Insert your name: ")
-    val name = readLine()
+    print("Enter your name: ")
+    val name = readlnOrNull()
     if (name != null) {
         println("Hello $name!")
     } else {
